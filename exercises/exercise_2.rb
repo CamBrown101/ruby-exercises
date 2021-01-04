1ea7ba93af4440052c2b462afadfbe626d7f9aab
cambrown101 <cameron-brown@live.com>
2021-01-04 13:44:47 -0800

    Change store1 name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -3,5 +3,11 @@ require_relative './exercise_1'
 
 puts "Exercise 2"
 puts "----------"
+@store1 = Store.first
+@store2 = Store.second
 
+
+@store1.name = "Boundry"
+puts @store1.name
+puts @store2.name
 # Your code goes here ...

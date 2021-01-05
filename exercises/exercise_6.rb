f8a17ac1cf89943d7a47c69e33f618e7ea098e27
cambrown101 <cameron-brown@live.com>
2021-01-04 18:33:47 -0800

    Add error messaging

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,7 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Cam", last_name: "Brown", hourly_rate: 100)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Sam", last_name: "Young", hourly_rate: 100)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,22 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+class Employees < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, length: {maximum: 70, minimium: 40}
+  validates :hourly_rate, numericality: { only_integer: true }
+  validates :store_id, presence: true
+end
+
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: {greater_than_or_equal: 0}
+end
+
+puts "Enter store name:"
+storeName = gets.chomp
+
+store = Store.create(name: storeName, annual_revenue: nil, mens_apparel: nil, womens_apparel: nil)
+store.valid?
+puts store.errors.full_messages
